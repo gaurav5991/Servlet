cf235923484e2ef1fe13dd3318216d0a9bcf372a
gaurav5991 <gauravharendersharma@gmail.com>
2020-11-28 00:00:56 +0530

    Gaurav[ADDED]:Created First Servlet Application using Maven

---
+++ src/main/java/com/capgemini/demo/SimpleServlet.java
@@ -0,0 +1,24 @@
+package com.capgemini.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class SimpleServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Hello World! How are you doing today!</h3>");
+		out.close();
+	}
+}
